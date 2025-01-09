b77ba049407649c87bc2789e176e37e9f11a1ade
Defrijay <defrijay@gmail.com>
2025-01-09 10:15:23 +0700

    benarkan teks hero

--- src/sections/MyHero.jsx
+++ src/sections/MyHero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Defrizal Yahdiyan Risyad</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop code for website and
+            I develop code for website and mobile.
           </p>
         </div>
       </div>
